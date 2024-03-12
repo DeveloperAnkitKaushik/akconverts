60680459ba5849147154d2bddd45875c4927246d
Ankit Kaushik <96423191+DeveloperAnkitKaushik@users.noreply.github.com>
2024-03-12 18:23:19 +0530

    minor changes

--- src/utils/convert.ts
+++ src/utils/convert.ts
@@ -1,15 +1,14 @@
-// imports
 import { Action } from "@/types.d";
 import { FFmpeg } from "@ffmpeg/ffmpeg";
 import { fetchFile } from "@ffmpeg/util";
 
 function getFileExtension(file_name: string) {
-  const regex = /(?:\.([^.]+))?$/; // Matches the last dot and everything after it
+  const regex = /(?:\.([^.]+))?$/; 
   const match = regex.exec(file_name);
   if (match && match[1]) {
     return match[1];
   }
-  return ""; // No file extension found
+  return "";
 }
 
 function removeFileExtension(file_name: string) {
@@ -17,7 +16,7 @@ function removeFileExtension(file_name: string) {
   if (lastDotIndex !== -1) {
     return file_name.slice(0, lastDotIndex);
   }
-  return file_name; // No file extension found
+  return file_name;
 }
 
 export default async function convert(
@@ -28,10 +27,7 @@ export default async function convert(
   const input = getFileExtension(file_name);
   const output = removeFileExtension(file_name) + "." + to;
   ffmpeg.writeFile(input, await fetchFile(file));
-
-  // FFMEG COMMANDS
   let ffmpeg_cmd: any = [];
-  // 3gp video
   if (to === "3gp")
     ffmpeg_cmd = [
       "-i",
@@ -55,10 +51,7 @@ export default async function convert(
       output,
     ];
   else ffmpeg_cmd = ["-i", input, output];
-
-  // execute cmd
   await ffmpeg.exec(ffmpeg_cmd);
-
   const data = (await ffmpeg.readFile(output)) as any;
   const blob = new Blob([data], { type: file_type.split("/")[0] });
   const url = URL.createObjectURL(blob);
